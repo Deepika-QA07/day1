254c2f965518485f05b180453afcde97e184ba15
Deepika-QA07 <kdeepikagowri@gmail.com>
2025-09-15 16:39:06 +0530

    Add files via upload
    Odd Numbers

---
+++ FindOddNumbers.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FindOddNumbers {
+	public static void main(String[] args) {
+		//Numbers from 1 to 10
+		for (int i=1; i<=10; i++){
+			//
+			if(i%2 !=0){
+				System.out.println("The Odd Numbers are " + i);
+			}
+			//else
+			//{
+				//System.out.println("The Even Numbers are" + " " + i);
+			//}
+	
+	}
+	}
+}
